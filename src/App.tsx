118e544c41c270e56ec67cf0b99658a745efd415
Josefin <josefinp94@gmail.com>
2023-04-07 10:08:22 +0200

    cleanup done

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,18 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      <h1>Josefin Persson</h1>
+
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      </div> */}
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,7 +29,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </div>
   )
 }
