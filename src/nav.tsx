4ed33ec5c225dd425cbd7da3ed2ee1e74db1f525
Josefin <josefinp94@gmail.com>
2023-04-11 08:09:02 +0200

    tailwind added

--- src/nav.tsx
+++ src/nav.tsx
@@ -1,24 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BsFillMoonStarsFill} from 'react-icons/bs'
 
 function Nav() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="Nav">
-      <div className="nav-container container">
-        <nav>
-            <ul>
-                <li><a>Home</a></li>
-                <li><a>About</a></li>
-                <li><a>Work</a></li>
-                <li><a>Contact</a></li>
+    <section>
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1 className='text-xl font-bold'>Josefin Persson</h1>
+            <ul className='flex items-center'>
+                <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
+                <li><a href="#" className='bg-gradient-to-r from-pink-700 to-pink-500 text-white px-4 py-2 rounded-md ml-8'>Hej</a></li>
+                {/* <li><a href="#" className='bg-gradient-to-r from-pink-700 to-pink-500 text-white px-4 py-2 rounded-md ml-8'>Home</a></li> */}
+                {/* <li><a href="#" className='bg-gradient-to-r from-green-700 to-green-500 text-white px-4 py-2 rounded-md ml-4'>About</a></li>
+                <li><a href="#" className='bg-gradient-to-r from-blue-700 to-blue-500 text-white px-4 py-2 rounded-md ml-4'>Work</a></li>
+                <li><a href="#" className='bg-gradient-to-r from-amber-700 to-amber-500 text-white px-4 py-2 rounded-md ml-4'>Contact</a></li> */}
             </ul>
         </nav>
-      </div>
-    </div>
+    </section>
   )
 }
 
